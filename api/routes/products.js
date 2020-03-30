b04e69a684dae28a40cbe85a13507d07278de2e3
alex.szeto <alex.szeto.a@gmail.com>
2020-03-30 14:27:10 -0400

    only one way to find out

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+
+const Product = require('../models/product.js')
 
 router.get('/', (req,res,next)=>{
     res.status(200).json({
@@ -8,19 +11,35 @@ router.get('/', (req,res,next)=>{
 })
 
 router.post('/', (req,res,next)=>{
-    const product = {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    }
+    });
+
+    product.save()
+    .then(result => {
+        console.log(result)
+    })
+    .catch(err => console.log(err))
+
     res.status(200).json({
         test: "Handling Post Requests to /products",
         createdProduct: product
     })
 })
 
-router.patch('/:productID', (req,res,next) => {
-    res.status(200).json({
-        message: "update product"
+router.get('/:productId', (req,res,next) => {
+    const id = req.params.productId
+
+    Product.findById(id)
+    .exec()
+    .then(doc => {
+        console.log(doc)
+        res.status(200).json("from database" + doc)
+    })
+    .catch(err => {
+        res.status(500).json({error: err})
     })
 })
 
--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const productRoutes = require('./api/routes/products.js')
 const orderRoutes = require('./api/routes/orders.js')
+
+mongoose.connect('mongodb+srv://Alex-Szeto:' + process.env.MONGO_ATLAS_PW + '@cluster0-3z397.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false}))
@@ -13,9 +16,14 @@ app.use(bodyParser.json())
 app.use('/orders', orderRoutes)
 app.use('/products', productRoutes)
 
-app.use((res,res,next)=>{
+app.use((req,res,next)=>{
     res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    if(req.method == "OPTIONS"){
+        res.header('Access-Control-Allow-Methods', "PUT, POST, PATCH, DELETE, GET")
+        return res.status(200).json({})
+    }
+    next()
 })
 
 app.use((req,res,next)=>{
